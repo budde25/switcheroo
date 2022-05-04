f46fce496e1027ce2797ff458cff378a199f68e0
Ethan Budd <budde25@protonmail.com>
2022-05-04 01:51:05 -0500

    clean up gui a bit more

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -9,14 +9,14 @@ use std::time::Duration;
 
 use color_eyre::eyre::Result;
 
-use egui::{Color32, RichText};
+use egui::{Button, Color32, Context, RichText, Ui};
 use image::Images;
 use tegra_rcm::{Error, Payload, Rcm};
 
+type ThreadSwitchResult = Arc<Mutex<Result<Rcm, Error>>>;
+
 pub fn gui() -> Result<()> {
-    let rcm = Rcm::new(false);
-    let arc = Arc::new(Mutex::new(rcm));
-    let arc_clone = arc.clone();
+    let rcm = Arc::new(Mutex::new(Rcm::new(false)));
 
     let images = Images::default();
 
@@ -30,23 +30,11 @@ pub fn gui() -> Result<()> {
         "Switcheroo",
         options,
         Box::new(|cc| {
-            let ctx = cc.egui_ctx.clone();
-            thread::spawn(move || loop {
-                {
-                    let lock = arc.lock();
-                    if let Ok(mut inner) = lock {
-                        let new = Rcm::new(false);
-                        *inner = new;
-                        ctx.request_repaint();
-                    }
-                }
-                thread::sleep(Duration::from_secs(1));
-            });
+            spawn_rcm_check_thread(rcm.clone(), cc.egui_ctx.clone(), Duration::from_secs(1));
 
             Box::new(MyApp {
-                switch: arc_clone,
+                switch: rcm,
                 payload_data: None,
-                executable: false,
                 images,
                 dark_mode: false,
                 state: State::NotAvailable,
@@ -60,17 +48,63 @@ struct MyApp {
     payload_data: Option<PayloadData>,
     images: Images,
     state: State,
-    executable: bool,
     dark_mode: bool,
+}
+
+impl MyApp {
+    // we can execute if we have a payload and rcm is available
+    fn executable(&self) -> bool {
+        // we can't be excutable in this state
+        match self.state {
+            State::NotAvailable => return false,
+            State::Available => (),
+            State::Done => return false,
+        };
+
+        // if we have a payload
+        if let Some(payload_data) = &self.payload_data {
+            return payload_data.payload.is_ok();
+        };
+        false
+    }
+
+    // get the payload its available
+    fn payload(&self) -> Option<&Payload> {
+        if let Some(payload_data) = &self.payload_data {
+            if let Ok(payload) = &payload_data.payload {
+                Some(payload)
+            } else {
+                None
+            };
+        };
+        None
+    }
+
+    /// Check if we need to change our current state
+    fn check_change_state(&mut self) {
+        if self.state == State::Done {
+            return;
+        }
+
+        let arc = self.switch.try_lock();
+        if let Ok(lock) = arc {
+            let res = &*lock;
+            match res {
+                Ok(_) => self.state = State::Available,
+                Err(_) => self.state = State::NotAvailable,
+            }
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum State {
-    Available,
     NotAvailable,
+    Available,
     Done,
 }
 
+#[derive(Debug)]
 struct PayloadData {
     payload: Result<Payload, Error>,
     picked_path: String,
@@ -79,39 +113,35 @@ struct PayloadData {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            // Title
             ui.label(RichText::new("Switcheroo").size(30.0).strong());
             ui.add_space(10.0);
+
             ui.group(|ui| {
                 ui.add_space(10.0);
                 if let Some(payload_data) = &self.payload_data {
-                    ui.horizontal(|ui| {
-                        if payload_data.payload.is_ok() {
-                            ui.label(RichText::new("Payload:").size(16.0));
-                            ui.monospace(
-                                RichText::new(&payload_data.picked_path)
-                                    .color(Color32::BLUE)
-                                    .size(16.0),
-                            );
-                        } else {
-                            ui.label(RichText::new("Error:").color(Color32::RED).size(16.0));
-                            let error = &payload_data.payload.as_ref().unwrap_err().to_string();
-                            ui.monospace(RichText::new(error).color(Color32::RED).size(16.0));
+                    match payload_data.payload {
+                        Ok(_) => {
+                            ui.horizontal(|ui| {
+                                ui.label(RichText::new("Payload:").size(16.0));
+                                ui.monospace(
+                                    RichText::new(&payload_data.picked_path)
+                                        .color(Color32::BLUE)
+                                        .size(16.0),
+                                );
+                            });
                         }
-                    });
-                    if self.state == State::Available {
-                        self.executable = true;
+                        Err(e) => create_error(ui, &e.to_string()),
                     }
                 } else {
                     ui.horizontal(|ui| {
                         ui.label(RichText::new("Payload:").size(16.0));
                         ui.monospace(RichText::new("None").size(16.0));
-                        self.executable = false;
                     });
                 }
+
                 ui.add_space(10.0);
-
                 ui.separator();
-
                 ui.add_space(10.0);
 
                 ui.vertical_centered(|ui| {
@@ -127,40 +157,19 @@ impl eframe::App for MyApp {
 
                         if ui
                             .add_enabled(
-                                self.executable,
-                                egui::Button::new(RichText::new("Execute").size(18.0)),
+                                self.executable(),
+                                Button::new(RichText::new("Execute").size(18.0)),
                             )
                             .clicked()
                         {
-                            let payload = self
-                                .payload_data
-                                .as_ref()
-                                .unwrap()
-                                .payload
-                                .as_ref()
-                                .unwrap();
+                            // we are safe to unwrap because we can only get the payload if we are executable
+                            let payload = self.payload().unwrap();
                             if let Ok(mut res) = self.switch.try_lock() {
                                 // TODO: fix race condition
                                 if let Ok(switch) = &mut *res {
                                     match execute(switch, payload) {
-                                        Ok(_) => {
-                                            self.state = State::Done;
-                                            self.executable = false;
-                                        }
-                                        Err(e) => {
-                                            ui.horizontal(|ui| {
-                                                ui.label(
-                                                    RichText::new("Error:")
-                                                        .color(Color32::RED)
-                                                        .size(18.0),
-                                                );
-                                                ui.monospace(
-                                                    RichText::new(e.to_string())
-                                                        .color(Color32::RED)
-                                                        .size(18.0),
-                                                );
-                                            });
-                                        }
+                                        Ok(_) => self.state = State::Done,
+                                        Err(e) => create_error(ui, &e.to_string()),
                                     }
                                 }
                             }
@@ -179,20 +188,10 @@ impl eframe::App for MyApp {
                     }
                     State::Done => self.images.done.show_max_size(ui, ui.available_size()),
                 };
-
-                if self.state != State::Done {
-                    let arc = self.switch.try_lock();
-                    if let Ok(lock) = arc {
-                        let res = &*lock;
-                        match res {
-                            Ok(_) => self.state = State::Available,
-                            Err(_) => self.state = State::NotAvailable,
-                        }
-                    }
-                }
             });
         });
 
+        self.check_change_state();
         preview_files_being_dropped(ctx);
 
         // Collect dropped files:
@@ -204,6 +203,14 @@ impl eframe::App for MyApp {
             }
         }
     }
+}
+
+/// Creates a basic error string
+fn create_error(ui: &mut Ui, error: &str) {
+    ui.horizontal(|ui| {
+        ui.label(RichText::new("Error:").color(Color32::RED).size(18.0));
+        ui.monospace(RichText::new(error).color(Color32::RED).size(18.0));
+    });
 }
 
 /// Preview hovering files
@@ -261,4 +268,19 @@ fn make_payload_data(path: &Path) -> Option<PayloadData> {
         return Some(payload_data);
     }
     None
+}
+
+/// Creates a backgroud thread that reports if there is a switch in RCM mode detected
+fn spawn_rcm_check_thread(tswitch: ThreadSwitchResult, ctx: Context, refresh: Duration) {
+    thread::spawn(move || loop {
+        {
+            let lock = tswitch.lock();
+            if let Ok(mut inner) = lock {
+                let new = Rcm::new(false);
+                *inner = new;
+                ctx.request_repaint();
+            }
+        }
+        thread::sleep(refresh);
+    });
 }
