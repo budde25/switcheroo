e46ce2652bfaa50eb681455a7b3a72d9bca95b18
Ethan Budd <budde25@protonmail.com>
2022-05-07 16:54:02 -0500

    added desktop file and icon

--- build.rs
+++ build.rs
@@ -1,16 +1,28 @@
 use clap::IntoApp;
 use clap_complete::generate_to;
 use clap_complete::Shell::{Bash, Fish, Zsh};
-use std::env;
+use std::fs;
 use std::io::Error;
+use std::path::Path;
+use std::{env, process};
 
 include!("src/cli.rs");
 
 fn main() -> Result<(), Error> {
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
+    let mut outdir = match env::var_os("CARGO_MANIFEST_DIR") {
+        None => {
+            eprintln!("CARGO_MANIFEST_DIR not defined");
+            process::exit(1);
+        }
+        Some(outdir) => PathBuf::from(outdir),
     };
+
+    app_dir(&outdir)?;
+
+    // we are gonna push to the extra dir
+    outdir.push("extra");
+    let mut completions = outdir.clone();
+    completions.push("completions");
 
     let shells = [Bash, Fish, Zsh];
     for shell in shells {
@@ -19,10 +31,17 @@ fn main() -> Result<(), Error> {
             shell,
             &mut cmd,     // We need to specify what generator to use
             "switcheroo", // We need to specify the bin name manually
-            &outdir,      // We need to specify where to write to
+            &completions, // We need to specify where to write to
         )?;
-        //println!("cargo:warning=completion file is generated: {:?}", path);
     }
 
+    Ok(())
+}
+
+/// Doesn't support another target dir
+fn app_dir(path: &Path) -> Result<(), Error> {
+    let target = path.join("target");
+    let bin = target.join("AppDir").join("usr").join("bin");
+    fs::create_dir_all(&bin)?;
     Ok(())
 }
