7812ff7a075e0deb155550e54e59c1c84d450afb
Ethan Budd <budde25@protonmail.com>
2022-05-28 12:59:17 -0500

    clean up hotplug code

--- crates/tegra-rcm/src/hotplug/common.rs
+++ crates/tegra-rcm/src/hotplug/common.rs
@@ -1,4 +1,4 @@
-use rusb::{Device, DeviceHandle, GlobalContext, HotplugBuilder, UsbContext};
+use rusb::{Device, DeviceHandle, GlobalContext, HotplugBuilder, UsbContext, has_hotplug, Hotplug};
 
 use super::Actions;
 use crate::device::{SwitchDevice, SWITCH_PID, SWITCH_VID};
@@ -17,36 +17,30 @@ struct HotplugHandler {
 }
 unsafe impl Send for HotplugHandler {}
 
-impl rusb::Hotplug<GlobalContext> for HotplugHandler {
+impl Hotplug<GlobalContext> for HotplugHandler {
     /// Gets called whenever a new usb device arrives
     fn device_arrived(&mut self, device: Device<GlobalContext>) {
-        if let Ok(device_desc) = device.device_descriptor() {
-            if device_desc.vendor_id() == SWITCH_VID && device_desc.product_id() == SWITCH_PID {
-                // if this is not Ok, it probably got unplugged really fast
-                if let Ok(dev) = device.open() {
-                    let rcm = Rcm::with_device_handle(dev);
-                    self.inner.arrives(rcm);
-                }
-            }
+        // if this is not Ok, it probably got unplugged really fast
+        if let Ok(dev) = device.open() {
+            let rcm = Rcm::with_device_handle(dev);
+            self.inner.arrives(rcm);
         }
     }
 
     /// Gets called whenever a usb device leaves
-    fn device_left(&mut self, device: Device<GlobalContext>) {
-        if let Ok(device_desc) = device.device_descriptor() {
-            if device_desc.vendor_id() == SWITCH_VID && device_desc.product_id() == SWITCH_PID {
-                self.inner.leaves();
-            }
-        }
+    fn device_left(&mut self, _device: Device<GlobalContext>) {
+        self.inner.leaves();
     }
 }
 
 /// create a hotplug setup, this blocks
 pub fn create_hotplug(data: Box<dyn Actions>) {
-    if rusb::has_hotplug() {
+    if has_hotplug() {
         let context = rusb::GlobalContext::default();
 
         let _hotplug = HotplugBuilder::new()
+            .vendor_id(SWITCH_VID)
+            .product_id(SWITCH_PID)
             .enumerate(true)
             .register(&context, Box::new(HotplugHandler { inner: data }))
             .expect("We where able to successfully wrap the context");
--- crates/tegra-rcm/src/rcm.rs
+++ crates/tegra-rcm/src/rcm.rs
@@ -128,8 +128,11 @@ impl Rcm {
     }
 
     fn trigger_controlled_memcopy(&self) -> Result<()> {
-        debug!("Wrote a total of {} bytes to the switch, performing the controlled memcopy", self.total_written);
-    
+        debug!(
+            "Wrote a total of {} bytes to the switch, performing the controlled memcopy",
+            self.total_written
+        );
+
         const STACK_END: usize = 0x40010000;
         let length = STACK_END - self.current_buff_addr();
         self.trigger_controlled_memcopy_length(length)?;
