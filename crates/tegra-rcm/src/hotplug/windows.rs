583de8bfb7082c10c9068a1cbbe896746399a02b
Ethan Budd <budde25@protonmail.com>
2022-05-29 10:31:22 -0500

    got windows support working!

--- crates/tegra-rcm/src/hotplug/windows.rs
+++ crates/tegra-rcm/src/hotplug/windows.rs
@@ -1,6 +1,6 @@
-use libusbk::DeviceHandle;
+use libusbk::{has_hotplug, Device, DeviceHandle, Hotplug, HotplugBuilder};
 
-use crate::device::{Device, SwitchDevice, SWITCH_PID, SWITCH_VID};
+use crate::device::{SwitchDevice, SWITCH_PID, SWITCH_VID};
 use crate::{Actions, Rcm};
 
 impl Rcm {
@@ -16,43 +16,36 @@ struct HotplugHandler {
 }
 unsafe impl Send for HotplugHandler {}
 
-impl libusbk::Hotplug for HotplugHandler {
+impl Hotplug for HotplugHandler {
     /// Gets called whenever a new usb device arrives
     fn device_arrived(&mut self, device: Device) {
-        if let Ok(device_desc) = device.device_descriptor() {
-            if device_desc.vendor_id() == SWITCH_VID && device_desc.product_id() == SWITCH_PID {
-                // if this is not Ok, it probably got unplugged really fast
-                if let Ok(dev) = device.open() {
-                    let rcm = Rcm::with_device_handle(dev);
-                    self.inner.arrives(rcm);
-                }
-            }
+        // if this is not Ok, it probably got unplugged really fast
+        if let Ok(dev) = device.open() {
+            let rcm = Rcm::with_device_handle(dev);
+            self.inner.arrives(rcm);
         }
     }
 
     /// Gets called whenever a usb device leaves
-    fn device_left(&mut self, device: Device) {
-        if let Ok(device_desc) = device.device_descriptor() {
-            if device_desc.vendor_id() == SWITCH_VID && device_desc.product_id() == SWITCH_PID {
-                self.inner.leaves();
-            }
-        }
+    fn device_left(&mut self, _device: Device) {
+        self.inner.leaves();
     }
 }
 
 /// create a hotplug setup, this blocks
 pub fn create_hotplug(data: Box<dyn Actions>) {
-    if libusk::has_hotplug() {
-        let context = rusb::GlobalContext::default();
-
-        let _hotplug = HotplugBuilder::new()
-            .enumerate(true)
-            .register(&context, Box::new(HotplugHandler { inner: data }))
+    if has_hotplug() {
+        let mut register = HotplugBuilder::new()
+            .vendor_id(SWITCH_VID as i32)
+            .product_id(SWITCH_PID as i32)
+            .register(Box::new(HotplugHandler { inner: data }))
             .expect("We where able to successfully wrap the context");
+
+        register.init().unwrap();
 
         loop {
             // blocks thread
-            context.handle_events(None).unwrap();
+            // context.handle_events(None).unwrap();
         }
     } else {
         panic!("libusbK hotplug API unsupported");
